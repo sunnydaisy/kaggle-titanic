07ca4bbc8078cf0a62709cb4bd016819b0e6b32a
sunnydaisy <47199328+sunnydaisy@users.noreply.github.com>
2019-07-22 18:15:50 +0900

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -60,7 +60,7 @@ test['Fare'].fillna(test.groupby('Pclass')['Fare'].transform('median'),inplace=T
 for dataset in train_test_data:
     dataset.loc[dataset['Fare']<=17,'Fare']=0,
     dataset.loc[(dataset['Fare']>17) & (dataset['Fare']<=30),'Fare']=1,
-    dataset.loc[(dataset['Fare']<=17) & (dataset['Fare']<=100),'Fare']=2,
+    dataset.loc[(dataset['Fare']>30) & (dataset['Fare']<=100),'Fare']=2,
     dataset.loc[dataset['Fare']>100,'Fare']=3
 
 for dataset in train_test_data:
